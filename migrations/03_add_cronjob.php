0b0d20d694667021c5c3e350b3e84d975ac2ec89
s-at-dq <strohm@data-quest.de>
2016-12-07 11:13:21 +0100

    Lizenzstatus: fixed migration 03

--- migrations/03_add_cronjob.php
+++ migrations/03_add_cronjob.php
@@ -42,12 +42,9 @@ class AddCronjob extends Migration {
                 "INSERT INTO `cronjobs_tasks`
                 (`task_id`, `filename`, `class`, `active`) 
                 VALUES
-                (:task_id, :filename, :class, '1')",
-                array(
-                    'task_id' => $this->cronjob_md5,
-                    'filename' => self::$cronjob['filename'],
-                    'class' => self::$cronjob['class']
-                )
+                ('".$this->cronjob_md5."', '"
+                .self::$cronjob['filename']."', '"
+                .self::$cronjob['class']."', '1')"
             );
             
             $schedule_id = md5(uniqid($this->cronjob_md5, true));
@@ -57,14 +54,10 @@ class AddCronjob extends Migration {
                 (`schedule_id`, `task_id`, `parameters`, `priority`,
                 `type`, `minute`, `mkdate`, `chdate`, `last_result`)
                 VALUES
-                (:schedule_id, :task_id, '[]', :priority,
-                'once', :minute, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), NULL)",
-                array(
-                    'schedule_id' => $schedule_id,
-                    'task_id' => $this->cronjob_md5,
-                    'priority' => self::$cronjob['priority'],
-                    'minute' => self::$cronjob['minute']
-                )
+                ('".$schedule_id."', '"
+                .$this->cronjob_md5."', '[]', '".self::$cronjob['priority']
+                ."', 'once', '".self::$cronjob['minute']
+                ."', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), NULL)"
             );
             
             $db = null;
@@ -78,17 +71,11 @@ class AddCronjob extends Migration {
             $db = DBManager::get();
             
             $db->exec(
-                "DELETE FROM `cronjobs_tasks` WHERE task_id = :cronjob_md5",
-                array(
-                    'cronjob_md5' => $this->cronjob_md5
-                )
+                "DELETE FROM `cronjobs_tasks` WHERE task_id = '".$this->cronjob_md5."'"
             );
             
             $db->exec(
-                "DELETE FROM `cronjobs_schedules` WHERE task_id = :cronjob_md5",
-                array(
-                    'cronjob_md5' => $this->cronjob_md5
-                )
+                "DELETE FROM `cronjobs_schedules` WHERE task_id = '".$this->cronjob_md5."'"
             );
             
             $db = null;
