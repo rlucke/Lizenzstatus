e02338d0bc30ef3afd095c969b96266a5563a5bb
Rasmus Fuhse <fuhse@data-quest.de>
2016-12-15 15:52:51 +0100

    fix 52a-text

--- migrations/01_init_plugin.php
+++ migrations/01_init_plugin.php
@@ -6,7 +6,7 @@ class InitPlugin extends Migration
         INSERT IGNORE INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) 
         VALUES
             (MD5('INFO_TEXT_52A'), '', 'INFO_TEXT_52A', '".
-                "Beachten Sie, dass Texte, die urheberrechtlich geschützt und nicht über Campuslizenzen abgedeckt sind, nicht mehr pauschal über die Hochschule vergütet wird. Wenn Sie einen derartigen Text hochladen, müssen Sie theoretisch selbst die VGWort benachrichtigen und die Abrechnung individuell vornehmen."
+                "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua."
                 ."', 0, 'string', 'global', 'global', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Wird im Dateibereich angezeigt für Dozenten.', '', '')
         ");
 	}
